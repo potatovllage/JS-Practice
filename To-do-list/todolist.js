3a29d00db550fa5ab5dbcf3a7f29e6457a98fae9
hohohogugugu <bluehome8626@naver.com>
2021-08-05 11:53:11 +0900

    To_do_list

--- To-do-list/todolist.js
+++ To-do-list/todolist.js
@@ -12,3 +12,23 @@ function addItem() {
 
     showList();
 }
+
+function showList() {
+    let list = "<ul>"
+    for (let i = 0; i < itemList.length; i++){
+        list += "<li>" + itemList[i] + "<span class='close' id=" + i + ">" + "\u00D7" + "</span></li>";
+    }
+    list += "</ul>";
+    document.querySelector(".tolist").innerHTML = list;
+
+    let deleteButtons = document.querySelectorAll(".close");
+    for (let i = 0; i < deleteButtons.length; i++){
+        deleteButtons[i].addEventListener("click", deleteItem);
+    }
+}
+
+function deleteItem() {
+    let id = this.getAttribute("id");
+    itemList.splice(id, 1);
+    showList();
+}
