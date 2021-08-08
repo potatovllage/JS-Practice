138fcf3eecd576212e150b748ef63841dada1aaa
hohohogugugu <bluehome8626@naver.com>
2021-08-08 14:44:14 +0900

    JS on The BROWSER

--- Nomadcoders vanilla JS 1/JS on The BROWSER/a1.js
+++ Nomadcoders vanilla JS 1/JS on The BROWSER/a1.js
@@ -1,6 +1,30 @@
+// HTML in Javascript
+/*
 const title = document.getElementById("title");
 
 title.innerText = "Got you!";
 
 console.log(title.id);
 console.log(title.className);
+*/
+
+// Searching For Elements 1
+/*
+ const hellos = document.getElementsByClassName("hello");
+
+ console.log(hellos);
+ */
+
+ // Searching For Elements 2
+ /*
+ const title = document.querySelector(".hello h1");
+
+ console.log(title);
+ */
+
+ // Searching For Elements 3
+ /*
+ const title = document.querySelector(".hello h1");
+
+ title.innerText = "Hello";
+ */
