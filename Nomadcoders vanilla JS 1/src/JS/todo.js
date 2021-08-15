cc00635723b76932f7c2da27ad4423741d78af0a
hohohogugugu <bluehome8626@naver.com>
2021-08-15 16:45:48 +0900

    Vanilla JS

--- Nomadcoders vanilla JS 1/src/JS/todo.js
+++ Nomadcoders vanilla JS 1/src/JS/todo.js
@@ -2,11 +2,17 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDo(event) {
+    const li = event.target.parentElement;
+    li.remove();
+}
+
 function paintToDo(newTodo) {
      const li = document.createElement("li");
      const span = document.createElement("span");
      const button = document.createElement("button");
      button.innerText = "❌";
+     button.addEventListener("click", deleteToDo);
      li.appendChild(span);
      li.appendChild(button);
      span.innerText = newTodo;
