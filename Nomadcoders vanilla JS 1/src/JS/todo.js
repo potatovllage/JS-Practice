fbdd2aa030ad3c76de9a013c430bc738cb862e4e
hohohogugugu <bluehome8626@naver.com>
2021-08-15 18:14:15 +0900

    Vanilla JS

--- Nomadcoders vanilla JS 1/src/JS/todo.js
+++ Nomadcoders vanilla JS 1/src/JS/todo.js
@@ -2,6 +2,14 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+const TODOS_KEY = "todos";
+
+const toDos = [];
+
+function saveToDos() {
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
 function deleteToDo(event) {
     const li = event.target.parentElement;
     li.remove();
@@ -23,7 +31,16 @@ function handleToDoSubmit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
+    toDos.push(newTodo);
     paintToDo(newTodo);
+    saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (saveToDos !== null) {
+    const parsedToDos = JSON.parse(saveToDos);
+    parsedToDos.array.forEach((item) => console.log("this is the turn of", item));
+}
