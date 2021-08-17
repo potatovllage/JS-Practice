5d1b5175f6acf8d510f53354598eac6ad3b150b6
hohohogugugu <bluehome8626@naver.com>
2021-08-18 08:41:16 +0900

    Vanilla JS

--- Nomadcoders vanilla JS 1/src/JS/quotes.js
+++ Nomadcoders vanilla JS 1/src/JS/quotes.js
@@ -1,47 +1,47 @@
 const quotes = [
     {
-      quote: "The way to get started is to quit talking and begin doing.",
+      quote: "The way to get started is to quit talking and begin doing -",
       author: "Walt Disney",
     },
     {
-      quote: "Life is what happens when you're busy making other plans.",
+      quote: "Life is what happens when you're busy making other plans -",
       author: "John Lennon",
     },
     {
       quote:
-        "The world is a book and those who do not travel read only one page.",
+        "The world is a book and those who do not travel read only one page -",
       author: "Saint Augustine",
     },
     {
-      quote: "Life is either a daring adventure or nothing at all.",
+      quote: "Life is either a daring adventure or nothing at all -",
       author: "Helen Keller",
     },
     {
-      quote: "To Travel is to Live",
+      quote: "To Travel is to Live -",
       author: "Hans Christian Andersen",
     },
     {
-      quote: "Only a life lived for others is a life worthwhile.",
+      quote: "Only a life lived for others is a life worthwhile -",
       author: "Albert Einstein",
     },
     {
-      quote: "You only live once, but if you do it right, once is enough.",
+      quote: "You only live once, but if you do it right, once is enough -",
       author: "Mae West",
     },
     {
-      quote: "Never go on trips with anyone you do ntot love.",
+      quote: "Never go on trips with anyone you do ntot love -",
       author: "Hemmingway",
     },
     {
-      quote: "We wander for distraction, but we travel for fulfilment.",
+      quote: "We wander for distraction, but we travel for fulfilment -",
       author: "Hilaire Belloc",
     },
     {
-      quote: "Travel expands the mind and fills the gap.",
+      quote: "Travel expands the mind and fills the gap -",
       author: "Sheda Savage",
     },
     {
-      quote: "Money begets money",
+      quote: "Money begets money -",
       author: "John Ray",
     },
   ];
